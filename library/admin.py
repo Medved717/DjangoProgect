724d2edf86241931d119da833fd26497ca8dddad
ВашеИмя <your_email@example.com>
2026-09-07 17:24:25 +0300

    Добавил админку.

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+from multiprocessing.reduction import register
 
-# Register your models here.
+from django.contrib import admin
+from library.models import Author, Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+
+    list_display = ('first_name', 'last_name', 'birth_date')
+    search_fields = ('first_name', 'last_name')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'publication_date', 'author')
+    list_filter = ('author',)
+    search_fields = ('title',)
+
