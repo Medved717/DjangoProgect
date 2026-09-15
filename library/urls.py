8e8b7c13068c8b9152aee4fbce8b3e3111714d75
ВашеИмя <your_email@example.com>
2026-09-15 16:19:31 +0300

    Изучил урок 24_5.

--- library/urls.py
+++ library/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'library'
 
 urlpatterns = [
-    path('books_list/', views.books_list, name='books_list'),
-    path('book_detail/<int:book_id>', views.book_detail, name='book_detail'),
+    path('library/books_list/', views.BooksListView.as_view(), name='books_list'),
+    path('library/books_form/', views.BooksCreateView.as_view(), name='books_create'),
+    path('library/books_form/<int:pk>/', views.BooksUpdateView.as_view(), name='books_update'),
+    path('library/books_confirm_delete/<int:pk>/', views.BooksDeleteView.as_view(), name='books_delete'),
+    path('library/books_detail/<int:pk>/', views.BooksDetailView.as_view(), name='books_detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.urls import reverse_lazy
+
 from library.models import Book, Author
+from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
 
 
-def books_list(requests):
-    books = Book.objects.all()
-    context = {'books': books}
-    return render(requests, 'library/books_list.html', context=context)
-
-def book_detail(request, book_id):
-    book = Book.objects.get(id=book_id)
-    context = {'book': book}
-    return render(request, 'library/book_detail.html', context=context)
+class BooksListView(ListView):
+    model = Book
+    template_name = 'library/books_list.html'
+    context_object_name = 'books'
 
 
+class BooksCreateView(CreateView):
+    model = Book
+    fields = ['title', 'publication_date', 'author']
+    template_name = 'library/books_form.html'
+    success_url = reverse_lazy('library:books_list')
+
+
+class BooksUpdateView(UpdateView):
+    model = Book
+    template_name = 'library/books_form.html'
+    fields = ['title', 'publication_date', 'author']
+    success_url = reverse_lazy('library:books_list')
+
+
+class BooksDeleteView(DeleteView):
+    model = Book
+    template_name = 'library/books_confirm_delete.html'
+    success_url = reverse_lazy('library:books_list')
+
+class BooksDetailView(DetailView):
+    model = Book
+    context_object_name = 'book'
+    template_name = 'library/books_detail.html'
